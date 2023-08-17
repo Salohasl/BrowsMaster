3894d9d75c0d14b391de34085fe105df6cec736d
Maxim Zavidiy <zavidiy1998@mail.ru>
2023-08-17 14:18:44 +0200

    version1

---
+++ script.js
@@ -0,0 +1,57 @@
+/* Для всплывающего окна */ 
+const modal = document.querySelector('#modal');
+const btn = document.querySelector('#signUp');
+const back = document.querySelector('#back');
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('nav');
+
+menuBtn.addEventListener('click', () =>{
+  menuBtn.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+btn.addEventListener('click', () =>{
+  modal.classList.toggle('active');
+  menuBtn.style.display = 'none';
+  menuBtn.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+back.addEventListener('click', () => {
+  menuBtn.style.display = 'block';
+  modal.classList.toggle('active');
+  menuBtn.classList.toggle('active');
+  menu.classList.toggle('active');
+})
+/* Для слайд шоу МОИ РАБОТЫ*/ 
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+    },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const buttonPrev = document.querySelector('.swiper-button-prev');
+const buttonNext = document.querySelector('.swiper-button-next');
+const slide = document.querySelector('.swiper');
+
+buttonPrev.addEventListener('mouseover', function(){
+  slide.classList.add('color');
+  buttonPrev.addEventListener('mouseout', function(){
+    slide.classList.remove('color');
+  })
+})
+buttonNext.addEventListener('mouseover', function(){
+  slide.classList.add('color');
+  buttonNext.addEventListener('mouseout', function(){
+    slide.classList.remove('color');
+  })
+})
